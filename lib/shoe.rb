db02b40e42d0af58d8d2dab55204a02424834948
Justin Movitz <movitzj1@montclair.edu>
2020-11-17 18:14:43 -0500

    Completed llab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :material, :size, :condition
+    #attr_reader :
+  
+    def initialize(brand)
+        @brand = brand
+    end
+  
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end 
